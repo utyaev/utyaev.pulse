117afc696131cb37bb0e10c43acaa595d028e5c1
Ilyas <utyaevis@gmail.com>
2025-06-17 20:54:46 +0300

    redesigned the slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,21 @@
-$(document).ready(function(){
-  $('.carousel__inner').slick({
-    speed: 1200,
-    prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
-  });
+// $(document).ready(function(){
+//   $('.carousel__inner').slick({
+//     speed: 1200,
+//     prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+//     nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+//   });
+// }); слайдер с помощью jquery
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: false
 });
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+})
